1c2974a490578e6ff8bad1a2294fe4cd35901bee
instigation <instigation@naver.com>
2020-08-30 20:15:21 +0900

    Implemented Json TreeView

--- JsonConverterTDD/JsonConverterTDD/MainWindow.xaml.cs
+++ JsonConverterTDD/JsonConverterTDD/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
+using System.ComponentModel;
+using Newtonsoft.Json.Linq;
 
 namespace JsonConverterTDD
 {
@@ -24,6 +27,50 @@ namespace JsonConverterTDD
         public MainWindow()
         {
             InitializeComponent();
+            UpdateTreeView(@"{'info':{'age':10,'name':'Smith'}}");
+        }
+
+        private void UpdateTreeView(string value)
+        {
+            try
+            {
+                var jTokenAdapter = new JTokenAdapter(JObject.Parse(value));
+                List<JTokenAdapter> items = new List<JTokenAdapter>();
+                items.Add(jTokenAdapter);
+                jsonTreeView.ItemsSource = items;
+            }
+            catch(Newtonsoft.Json.JsonReaderException e)
+            {
+                return;
+            }
+        }
+
+        private void targetTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var htmlText = new HtmlText(targetTextBox.Text);
+            string normalText = htmlText.GetDecodedText();
+            UpdateTreeView(normalText);
+        }
+    }
+
+    public class TemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate RootTemplate { get; set; }
+        public DataTemplate IntermediateTemplate { get; set; }
+        public DataTemplate LeafTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            JTokenAdapter adapter = item as JTokenAdapter;
+            switch (adapter.GetDataType())
+            {
+                case JTokenAdapter.DataType.Root:
+                    return RootTemplate;
+                case JTokenAdapter.DataType.Intermediate:
+                    return IntermediateTemplate;
+                case JTokenAdapter.DataType.Leaf:
+                default:
+                    return LeafTemplate;
+            }
         }
     }
 
@@ -40,5 +87,114 @@ namespace JsonConverterTDD
         }
 
         private string htmlText;
+    }
+
+    public class JTokenAdapter: TreeViewItemBase
+    {
+        public JTokenAdapter(JToken jToken)
+        {
+            Children = GetPropertyAndValuesFrom(jToken);
+        }
+
+        public JTokenAdapter(string property, JToken value)
+        {
+            Property = property;
+            switch (value.Type)
+            {
+                case JTokenType.Object:
+                    Children = GetPropertyAndValuesFrom(value);
+                    break;
+                case JTokenType.Array:
+                    Children = GetPropertyAndValuesFrom(value);
+                    break;
+                default:
+                    Value = value;
+                    break;
+            }
+        }
+
+        public enum DataType
+        {
+            Root,
+            Intermediate,
+            Leaf
+        };
+
+        public DataType GetDataType()
+        {
+            if (Property == null)
+                return DataType.Root;
+            else if (Children != null)
+                return DataType.Intermediate;
+            else
+                return DataType.Leaf;
+        }
+
+        private static ObservableCollection<JTokenAdapter> GetPropertyAndValuesFrom(JToken jToken)
+        {
+            var ret = new ObservableCollection<JTokenAdapter>();
+            JObject jObject = jToken.ToObject<JObject>();
+            foreach (var propertyAndValue in jObject)
+            {
+                string property = propertyAndValue.Key;
+                JToken value = propertyAndValue.Value;
+                ret.Add(new JTokenAdapter(property, value));
+            }
+            return ret;
+        }
+
+        public string Property { get; set; }
+
+        public JToken Value { get; set; }
+
+        public string ValueToString {
+            get {
+                if (Value is null)
+                    return "";
+
+                if(Value.Type == JTokenType.String)
+                    return "'" + Value.ToString() + "'";
+
+                return Value.ToString();
+            }
+        }
+
+        public ObservableCollection<JTokenAdapter> Children { get; set; }
+    }
+
+    public class TreeViewItemBase : INotifyPropertyChanged
+    {
+        private bool isSelected;
+        public bool IsSelected {
+            get { return this.isSelected; }
+            set {
+                if (value != this.isSelected)
+                {
+                    this.isSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        private bool isExpanded;
+        public bool IsExpanded {
+            get { return this.isExpanded; }
+            set {
+                if (value != this.isExpanded)
+                {
+                    this.isExpanded = value;
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
     }
 }
--- JsonConverterTDD/JsonConverterTests/JsonConverterTests.cs
+++ JsonConverterTDD/JsonConverterTests/JsonConverterTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JsonConverterTDD;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace JsonConverterTests
 {
@@ -12,6 +14,89 @@ namespace JsonConverterTests
             HtmlText doubleQuoteIncludedText = new HtmlText("&quot;Hello&quot;");
             string expectedDecodeResult = "\"Hello\"";
             Assert.AreEqual(expectedDecodeResult, doubleQuoteIncludedText.GetDecodedText());
+        }
+
+        [TestMethod]
+        public void TestJTokenAdapterConstructorFromJToken()
+        {
+            JToken jToken = JToken.Parse(@"{'age':10,'name':'Alice'}");
+            JTokenAdapter adapter = new JTokenAdapter(jToken);
+            Assert.AreEqual(2, adapter.Children.Count);
+        }
+
+        [TestMethod]
+        public void TestJTokenAdapterForObject()
+        {
+            JToken jToken = JToken.Parse(@"{'info':{'age':10,'name':'Smith'}}");
+            JTokenAdapter adapter = new JTokenAdapter(jToken);
+            Assert.AreEqual(1, adapter.Children.Count);
+            foreach(var infoChild in adapter.Children)
+            {
+                Assert.AreEqual("info", infoChild.Property);
+                Assert.AreEqual(2, infoChild.Children.Count);
+                var properties = new List<string>{ "age", "name"};
+                var values = new List<JToken>{ new JValue(10), new JValue("Smith")};
+                foreach(var child in infoChild.Children)
+                {
+                    Assert.IsTrue(properties.Contains(child.Property));
+                    Assert.IsTrue(values.Contains(child.Value));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestJTokenAdapterForIntValue()
+        {
+            JTokenAdapter adapter = new JTokenAdapter("age", new JValue(10));
+            Assert.AreEqual("10", adapter.ValueToString);
+        }
+
+        [TestMethod]
+        public void TestJTokenAdapterForBoolValue()
+        {
+            JTokenAdapter adapter = new JTokenAdapter("married", new JValue(false));
+            Assert.AreEqual(JTokenType.Boolean, adapter.Value.Type);
+            Assert.AreEqual("False", adapter.ValueToString);
+        }
+
+        [TestMethod]
+        public void TestJTokenAdapterForStringValue()
+        {
+            JTokenAdapter adapter = new JTokenAdapter("name", new JValue("Smith"));
+            Assert.AreEqual("'Smith'", adapter.ValueToString);
+        }
+    }
+
+    [TestClass]
+    public class NewtonSoftJsonTests
+    {
+        [TestMethod]
+        public void TestJObjectParse()
+        {
+            string jsonString = "{\"age\":\"10\"}";
+            JObject o = JObject.Parse(jsonString);
+            Assert.AreEqual("10", o["age"]);
+        }
+
+        [TestMethod]
+        public void TestJObjectEnumerate()
+        {
+            string jsonString = "{\"age\":\"10\", 'name':'minsu'}";
+            JObject o = JObject.Parse(jsonString);
+            var attributes = new List<KeyValuePair<string, JToken>>(o);
+            Assert.IsTrue(attributes.Contains(new KeyValuePair<string, JToken>("age", "10")));
+            Assert.IsTrue(attributes.Contains(new KeyValuePair<string, JToken>("name", "minsu")));
+        }
+
+        [TestMethod]
+        public void TestJTokenType()
+        {
+            string jsonString = "{'info':{\"age\":10, 'name':'minsu'}}";
+            JObject jObject = JObject.Parse(jsonString);
+            JToken infoToken = jObject["info"];
+            Assert.AreEqual(infoToken.Type, JTokenType.Object);
+            JToken ageToken = infoToken["age"];
+            Assert.AreEqual(ageToken.Type, JTokenType.Integer);
         }
     }
 }
